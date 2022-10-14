dce3167be2c164141a32d8dd30bad84d5a095d1d
Badutmooo <your.masby640@gmail.com>
2022-10-14 10:35:29 +0700

    C++ Coding

---
+++ Menghitung Tegangan V/src/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main (){
+  float V,I,R;
+  cout << "Masukkan I : ";
+  cin >> I;
+  cout << "Masukkan R : ";
+  cin >> R;
+  V= I * R;
+  cout << endl << "Hasilnya Adalah : " << V;
+}
